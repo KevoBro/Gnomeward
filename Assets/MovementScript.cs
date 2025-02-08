3eeea0bea8de71881415a3431d2208472b764f4b
KevoBro <91642406+KevoBro@users.noreply.github.com>
2025-02-07 21:59:59 -0500

    Initial commit

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public float movementSpeed; 
+    float speedX, speed Y; 
+    rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        
+    }
+
+    void FixedUpdate() {
+        
+
+    }
+}
