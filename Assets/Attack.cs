6b90541e9e0f141ebd12fae800fb1d7f68e16238
KevoBro <91642406+KevoBro@users.noreply.github.com>
2025-02-08 02:06:37 -0500

    Added Slash Attack

--- Assets/Attack.cs
+++ Assets/Attack.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform attackPoint;
+    public GameObject slashPrefab;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1")) {
+            Slash();
+
+        }
+    }
+
+    void Slash() {
+        GameObject slash = Instantiate(slashPrefab, attackPoint.position + new Vector3(0, .3f, 0), (attackPoint.rotation * Quaternion.Euler(0, 0, 90f))); 
+        Rigidbody2D rb = slash.GetComponent<Rigidbody2D>();
+
     }
 }
